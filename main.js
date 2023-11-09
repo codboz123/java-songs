ce7056d16cd06c895075d868f96ed112f809d84e
codboz123 <106533407+codboz123@users.noreply.github.com>
2023-11-09 08:16:13 -0300

    f

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("Olá segundo ano mais lindo desse mundo!!");
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+const ListaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+while(contador<ListaDeTeclas.length){
+    const efeito = ListaDeTeclas[contador].classList{1};
+    const idAudio = "#som_"+efeito;
+    ListaDeTeclas[contador].onclick = function(){
+        tocaSom("#som_tecla_aplausos");
+        tocaSom(idAudio);
+    }
+    contador = contador +1;
+    console.log(contador);
+}
